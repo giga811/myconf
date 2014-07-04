ddf8b1186368113e9918a2a0f0a08a7d65c16619
Spica <spica@Spica-no-MacBook-Pro.local>
2014-07-04 14:07:35 +0900

    Added eclipe, shellscripts

---
+++ shellscripts/clock_disp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# displays clock using "watch", "date", "figlet"
+
+# 引数
+if [ $# -gt 1 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "  usage: $0 <font name>" 1>&2
+  echo "  fontnames: script(default), big, mini, block, smcript" 1>&2
+  exit 1
+fi
+
+# default font
+font=script
+if [ $# -eq 1 ]; then
+	font=$1
+fi
+
+# fonts list
+fonts=("banner.flf  bubble.flf   lean.flf      script.flf  small.flf     smslant.flf\
+ big.flf     digital.flf  mini.flf      shadow.flf  smscript.flf  standard.flf\
+ block.flf   ivrit.flf    mnemonic.flf  slant.flf   smshadow.flf  term.flf")
+
+# commands
+date="date +%T"
+figlet="figlet -c -W -f $font"
+watch="watch -t -n1"
+
+$watch "$date | $figlet"
