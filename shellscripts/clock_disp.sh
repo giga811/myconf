8a02130ec0f7d754386e4d3eb8ea25586882ce29
Gigav <giga811@gmail.com>
2014-08-11 22:55:57 +0900

    Added bash profile for mac

--- shellscripts/clock_disp.sh
+++ shellscripts/clock_disp.sh
@@ -2,17 +2,23 @@
 # displays clock using "watch", "date", "figlet"
 
 # 引数
-if [ $# -gt 1 ]; then
+if [ $# -gt 2 ]; then
   echo "指定された引数は$#個です。" 1>&2
-  echo "  usage: $0 <font name>" 1>&2
+  echo "  usage: $0 <font name> <date option>" 1>&2
   echo "  fontnames: script(default), big, mini, block, smcript" 1>&2
+  echo "  date option: +%T(default)" 1>&2
   exit 1
 fi
 
 # default font
 font=script
+date_option="+%T"
 if [ $# -eq 1 ]; then
 	font=$1
+fi
+if [ $# -eq 2 ]; then
+	font=$1
+	date_option=$2
 fi
 
 # fonts list
@@ -21,7 +27,7 @@ fonts=("banner.flf  bubble.flf   lean.flf      script.flf  small.flf     smslant
  block.flf   ivrit.flf    mnemonic.flf  slant.flf   smshadow.flf  term.flf")
 
 # commands
-date="date +%T"
+date="date $date_option"
 figlet="figlet -W -f $font"
 watch="watch -t -n1"
 
